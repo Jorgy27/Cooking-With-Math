901066808e6191788dda30abd3110885bae926bd
George Trachanas <tracha27@windowslive.com>
2021-10-31 14:43:11 +0200

    Finished Second Scenario
    Added extensibility to the game by using a queue in case there is a need to add more that one question after each scenario. Also finished the second scenario and changed the mainUI script to check if there are any questions before showing the questionnaire or just let the player continue on the next scenario.

--- Assets/Scripts/MainUiHandler.cs
+++ Assets/Scripts/MainUiHandler.cs
@@ -8,18 +8,19 @@ using UnityEngine.SceneManagement;
 public class MainUiHandler : MonoBehaviour
 {
     [SerializeField]
-    private GameObject recipePanel;
+    private GameObject RecipePanel;
 
     [SerializeField]
-    private GameObject questionnairePanel;
+    private GameObject QuestionnairePanel;
 
     [SerializeField]
-    private GameObject failedTaskPanel;
+    private GameObject FailedTaskPanel;
+
+    [SerializeField]
+    private GameObject CompletedTaskPanel;
 
     [SerializeField]
     private CraftingRecipe recipe;
-
-    
 
     private void Start()
     {
@@ -29,34 +30,41 @@ public class MainUiHandler : MonoBehaviour
     //enable and load the recipe panel
     public void showRecipe()
     {
-        recipePanel.SetActive(true);
+        RecipePanel.SetActive(true);
         List<Items> materials = recipe.materials;
         string materialsText = "";
         foreach (Items item in materials)
         {
             materialsText += item.itemAmountLabel + " " + item.itemName + "\n";
         }
-        recipePanel.GetComponentInChildren<TextMeshProUGUI>().SetText(materialsText);
+        RecipePanel.GetComponentInChildren<TextMeshProUGUI>().SetText(materialsText);
         GameObject.Find("RecipeTitle").GetComponent<Text>().text = recipe.name;
     }
 
     public void hideRecipe()
     {
-        recipePanel.SetActive(false);
+        RecipePanel.SetActive(false);
     }
 
     public void onCraftClick(CraftingRecipe recipe)
     {
+        //get the full list of questions and answers from the QuestionnaireHandler
+        List<QuestionAndAnswer> qna = QuestionnairePanel.GetComponent<QuestionnaireHandler>().QnA;
+
         //enable the questionnaire panel
         bool canBeCrafted = recipe.CanCraft();
 
-        if (canBeCrafted)
+        if (canBeCrafted && qna.Count!=0)
         {
-            questionnairePanel.SetActive(true);
+            QuestionnairePanel.SetActive(true);
+        }
+        else if(canBeCrafted && qna.Count == 0)
+        {
+            CompletedTaskPanel.SetActive(true);
         }
         else
         {
-            failedTaskPanel.SetActive(true);
+            FailedTaskPanel.SetActive(true);
         }
     }
 
--- Assets/Scripts/QuestionnaireHandler.cs
+++ Assets/Scripts/QuestionnaireHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -17,19 +18,35 @@ public class QuestionnaireHandler : MonoBehaviour
 
     private GameObject QuestionnairePanel;
 
+    private Queue<QuestionAndAnswer> QnAQueue = new Queue<QuestionAndAnswer>();
+
     public void Start()
     {
         QuestionnairePanel = GameObject.Find("QuestionnairePanel");
+
+        //set the first question from the existing serialized list
         QuestionnairePanel.GetComponentInChildren<TextMeshProUGUI>().SetText(QnA[0].question);
+
+        //store the questions with the corresponding answers on a queue for better Extensibility
+        foreach (QuestionAndAnswer qna in QnA)
+        {
+            Debug.Log(qna.question);
+            QnAQueue.Enqueue(qna);
+        }
+
     }
 
     public void onAnwer(Text answerHolder)
     {
         string answer = answerHolder.text;
-        if (answer.Equals(QnA[0].answer))
+        QuestionAndAnswer questionAndAnswer = QnAQueue.Peek(); //gets the first item without removing it
+        if (answer.Equals(questionAndAnswer.answer))
         {
             CompletedTaskPanel.SetActive(true);
             QuestionnairePanel.SetActive(false);
+            questionAndAnswer = QnAQueue.Dequeue(); //get the first item and remove it from the queue
+
+            //TODO:set the next question if exists
         }
         else
         {
